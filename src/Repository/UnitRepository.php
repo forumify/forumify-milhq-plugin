98cdebcc590a57e96dca4d5cc4cef95dc7290a85
jannes <jannesdrijkoningen@gmail.com>
2026-07-12 20:44:32 +0200

    add supervisors can approve submissions

--- src/Repository/UnitRepository.php
+++ src/Repository/UnitRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Forumify\Milhq\Repository;
 
 use Forumify\Core\Repository\AbstractRepository;
+use Forumify\Milhq\Entity\Soldier;
 use Forumify\Milhq\Entity\Unit;
 
 /**
@@ -15,5 +16,18 @@ class UnitRepository extends AbstractRepository
     public static function getEntityClass(): string
     {
         return Unit::class;
+    }
+
+    public function findBySoldierIsSupervisor(Soldier|int $soldier): array
+    {
+        $soldierId = $soldier instanceof Soldier ? $soldier->getId() : $soldier;
+
+        return $this->createQueryBuilder('e')
+            ->innerJoin('e.supervisors', 'supervisor')
+            ->innerJoin(Soldier::class, 'soldier', 'WITH', 'soldier.position = supervisor AND soldier.unit = e')
+            ->where('soldier.id = :soldierId')
+            ->setParameter('soldierId', $soldierId)
+            ->getQuery()
+            ->getResult();
     }
 }
